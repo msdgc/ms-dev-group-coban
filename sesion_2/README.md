34896b7fef95f51a8f1bb4773a09216d52f3f0e8
Geovani de León <erwingeo1021@gmail.com>
2020-03-25 12:11:55 -0600

    Sesión #2

---
+++ sesion_2/README.md
@@ -0,0 +1,5 @@
+# Microsoft Dev Group Cobán - Sesión 2
+## .NET Core MVC vs Razor Pages
+
+## Síguenos
+[![N|Solid](../img/fb_icon.png)](https://www.facebook.com/groups/MsDevGroupCoban/)
---
+++ sesion_2/mvcApp1/Controllers/PersonasController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvcApp1.Models;
+
+namespace mvcApp1.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private readonly ILogger<PersonasController> _logger;
+
+        public PersonasController(ILogger<PersonasController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
